0d724bbf83d72ac09bda960ff55852edc0715e18
deaxmachina <d.y.bankova@gmail.com>
2021-11-07 16:19:47 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Header from "./components/Header.svelte"
+  import Footer from "./components/Footer.svelte"
+  import Tabs from "./shared/Tabs.svelte"
+  import CreatePollForm from "./components/CreatePollForm.svelte"
+
+  // tabs //
+  let items = ['Current Polls', 'Add New Poll']
+  let activeItem = 'Current Polls'
+
+  const tabChange = (e) => {
+    activeItem = e.detail
+  }
+
+</script>
+
+<Header />
+<main>
+  <Tabs 
+    items={items} activeItem={activeItem}
+    on:tabChange={tabChange}
+  />
+  {#if activeItem === 'Current Polls'}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+    <CreatePollForm />
+  {/if}
+</main>
+<Footer />
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
